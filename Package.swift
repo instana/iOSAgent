3a312c19619674201e10c7d2043ebc4c26021911
Christian Menschel <christian@tapwork.de>
2019-11-26 12:04:30 +0100

    Update naming

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "iOSSensor",
+    name: "InstanaSensor",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
