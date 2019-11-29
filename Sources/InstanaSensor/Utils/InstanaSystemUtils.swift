37743be1adba17957069dc90e4fa34c6ed4ea663
Christian Menschel <christian@tapwork.de>
2019-11-29 11:17:24 +0100

    Add no cellular if not having network

--- Sources/InstanaSensor/Utils/InstanaSystemUtils.swift
+++ Sources/InstanaSensor/Utils/InstanaSystemUtils.swift
@@ -81,7 +81,7 @@ class InstanaSystemUtils {
     }()
 
     static var connectionTypeDescription: String {
-        return (InstanaNetworkMonitor.shared.connectionType?.rawValue ?? "") + " - " + (cellularConnectionType ?? "")
+        return (InstanaNetworkMonitor.shared.connectionType?.rawValue ?? "") + " - " + (cellularConnectionType ?? "no cellular")
     }
     
     /// Returns a ' > ' sepparated string of view controller class names in the app hierarchy.
