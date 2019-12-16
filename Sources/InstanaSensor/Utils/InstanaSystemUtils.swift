f9ebe32915299f669b32d24feda435508405e928
Christian Menschel <christian@tapwork.de>
2019-12-16 10:27:22 +0100

    Move code into extension

--- Sources/InstanaSensor/Utils/InstanaSystemUtils.swift
+++ Sources/InstanaSensor/Utils/InstanaSystemUtils.swift
@@ -55,33 +55,6 @@ class InstanaSystemUtils {
     /// This is only a superficial check, and doesn't go deeper than one level.
     static func viewControllersHierarchy() -> String? {
         guard Thread.current.isMainThread else { return nil }
-        // This is tricky for multiple window support. which window should we use? TODO: Find a better solution
-        // For now we use all rootViewController)
-        let hierarchies = UIApplication.shared.windows.compactMap {window -> String? in
-            guard let root = window.rootViewController else { return nil }
-            var vcs = [UIViewController]()
-            let rootName = root.hiercharchyName
-
-            switch root {
-            case let nvc as UINavigationController:
-                vcs.append(contentsOf: nvc.viewControllers)
-            case let tvc as UITabBarController:
-                if let selected = tvc.selectedViewController {
-                    vcs.append(selected)
-                }
-            case let svc as UISplitViewController:
-                vcs.append(contentsOf: svc.viewControllers)
-            default: break
-            }
-
-            if let modal = (vcs.last ?? root).presentedViewController {
-                vcs.append(modal)
-            }
-            return vcs
-            .map { $0.hiercharchyName }
-            .reduce(rootName) { "\($0) > \($1)" }
-        }
-        let result = hierarchies.joined(separator: "\n")
-        return result.isEmpty ? nil : result
+        return UIWindow.viewControllerHierarchies
     }
 }
