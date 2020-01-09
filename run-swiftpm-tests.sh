451c3b03aad3d0828017960ee0be07d1535b5830
Christian Menschel <christian@tapwork.de>
2020-01-09 18:04:59 +0100

    Remove testplan since they don't work well with SwiftPM

--- run-swiftpm-tests.sh
+++ run-swiftpm-tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-
-xcodebuild test -destination 'name=iPhone 11' -scheme 'InstanaAgentTests' test -testPlan InstanaAgentTests | xcpretty
-xcodebuild test -destination 'name=iPhone 11' -scheme 'InstanaAgentTests' test -testPlan InstanaAgentIntegrationTests | xcpretty
-
+set -o pipefail
+xcodebuild test -destination 'name=iPhone 11' -scheme 'InstanaAgentTests' | xcpretty
+xcodebuild test -destination 'name=iPhone 11' -scheme 'InstanaAgentIntegrationTests' | xcpretty
