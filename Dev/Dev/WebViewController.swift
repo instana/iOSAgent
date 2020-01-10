e0c618dce4d0afa029fba26a9a53ec62bfa1330c
Christian Menschel <christian@tapwork.de>
2020-01-10 16:27:45 +0100

    Load many request at launch for dev app for testing purpose

--- Dev/Dev/WebViewController.swift
+++ Dev/Dev/WebViewController.swift
@@ -14,12 +14,21 @@ class WebViewController: UIViewController {
     override func loadView() {
         view = webView
         webView.load(URLRequest(url: URL(string: "https://www.instana.com")!))
+        loadLotsOfRequests()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if !isRunningTests {
             Instana.setView(name: "WebView: Instana.com")
+        }
+    }
+
+    func loadLotsOfRequests() {
+        (0...99).forEach {_ in
+            let sessi = URLSession(configuration: URLSessionConfiguration.default)
+            sessi.dataTask(with: URL(string: "https://www.spiegel.de")!) { (_, _, _) in
+            }.resume()
         }
     }
 }
