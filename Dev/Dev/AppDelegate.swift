d2d13293261f6ff7dd48804f5cc49a72d48d39a1
Christian Menschel <christian@tapwork.de>
2020-01-14 17:54:28 +0100

    Fix example build

--- Dev/Dev/AppDelegate.swift
+++ Dev/Dev/AppDelegate.swift
@@ -12,7 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             Instana.setMeta(value: "Value", key: "KEY")
             Instana.setMeta(value: "DEBUG", key: "Env")
             Instana.setUser(id: UUID().uuidString, email: "email@example.com", name: "Christian")
-            Instana.ignoreURL(matching: [".*http:.*"])
         }
         return true
     }
