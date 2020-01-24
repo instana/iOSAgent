c6208fa658f23ea6f0c5d2fd945e9be6199f1655
Christian Menschel <christian@tapwork.de>
2020-01-24 17:06:33 +0100

    Add script to run all tests

---
+++ scripts/run-all-test.sh
@@ -0,0 +1,7 @@
+./scripts/run-swiftpm-tests.sh --ios=13 --test=unit
+./scripts/run-swiftpm-tests.sh --ios=12 --test=unit
+./scripts/run-swiftpm-tests.sh --ios=11 --test=unit
+
+./scripts/run-swiftpm-tests.sh --ios=13 --test=integration
+./scripts/run-swiftpm-tests.sh --ios=12 --test=integration
+./scripts/run-swiftpm-tests.sh --ios=11 --test=integration
