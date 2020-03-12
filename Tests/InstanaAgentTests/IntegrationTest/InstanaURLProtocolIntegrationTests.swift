c7555515c1fba17018517c4931eddf9c1aa5a40f
Christian Menschel <christian@tapwork.de>
2020-03-12 18:33:34 +0100

    Remove WKWebView from automatic monitoring
    This is not reliable since WKWebView and WebKit run in a different process.
    There is an IPC between the app and WebKit process but some data (i.e.e POST body) are not transmitted to the app process.
    This would lead to unexpected behaviour. POST data would be discarded for instance.

--- Tests/InstanaAgentTests/IntegrationTest/InstanaURLProtocolIntegrationTests.swift
+++ Tests/InstanaAgentTests/IntegrationTest/InstanaURLProtocolIntegrationTests.swift
@@ -86,29 +86,6 @@ class InstanaURLProtocolIntegrationTests: InstanaTestCase {
         AssertEqualAndNotNil(resultBeacon?.url, givenURL)
     }
 
-    func test_urlprotocol_with_Webview_mock_report() {
-        // Given
-        let didReportWait = expectation(description: "didFinish")
-        var resultBeacon: HTTPBeacon?
-        let mockReporter = MockReporter { submittedBeacon in
-            if let httpBeacon = submittedBeacon as? HTTPBeacon {
-                resultBeacon = httpBeacon
-                didReportWait.fulfill()
-            }
-        }
-        let monitors = Monitors(session, reporter: mockReporter)
-        Instana.current = Instana(configuration: session.configuration, monitors: monitors)
-        let webView = WKWebView()
-
-        // When
-        webView.load(URLRequest(url: givenURL))
-        wait(for: [didReportWait], timeout: 10)
-
-        // Then
-        AssertEqualAndNotNil(resultBeacon?.url, givenURL)
-    }
-
-
     // MARK: Helper
     class SecondURLProtocol: URLProtocol {
         static var monitoredURL: URL?
