e7f25b7b38d8c63701fbb59c29bbb0d9f938fbfc
Christian Menschel <christian@tapwork.de>
2020-05-19 17:16:26 +0200

    Fix parsing server-timing

--- Sources/InstanaAgent/Utils/Extensions/URLResponse+URLRequest+Helper.swift
+++ Sources/InstanaAgent/Utils/Extensions/URLResponse+URLRequest+Helper.swift
@@ -1,16 +1,38 @@
 import Foundation
 
 extension URLResponse {
+    struct Pair {
+        let key: String
+        let value: String
+    }
     var backendTracingID: String? {
         guard let httpResponse = self as? HTTPURLResponse,
-            // can be lowercase TODO
-            let servertiming = httpResponse.allHeaderFields["Server-Timing"] as? String,
-            let regex = try? NSRegularExpression(pattern: "^intid;desc=(.+)$", options: .caseInsensitive),
-            let match = regex.firstMatch(in: servertiming, options: [], range: NSRange(location: 0, length: servertiming.utf16.count)),
-            match.numberOfRanges > 1,
-            let range = Range(match.range(at: 1), in: servertiming)
-        else { return nil }
+            let servertiming = httpResponse.value(for: "Server-Timing")
+            else { return nil }
 
-        return String(servertiming[range])
+        let items = servertiming.components(separatedBy: ", ")
+        let pairs = items.map {item -> Pair? in
+            let comps = item.components(separatedBy: "=")
+            guard let key = comps.first, let value = comps.last else { return nil}
+            return Pair(key: key, value: value)
+        }
+        let value = pairs.compactMap {$0}.last(where: {$0.key == "intid;desc"})?.value ?? ""
+        return !value.isEmpty ? value : nil
+    }
+}
+
+extension HTTPURLResponse {
+    func value(for field: String) -> String? {
+        // HTTP headerfields are defined to be insensitive - but not in Swift's HTTPURLResponse.
+        // Accessing HTTPURLResponse's headerfields is case-sensitive, which is wrong.
+        // see: https://bugs.swift.org/browse/SR-2429
+        // We could use: httpResponse.value(forHTTPHeaderField: "Server-Timing") - but this is only available since iOS 13
+        // https://bugs.swift.org/browse/SR-2429?focusedCommentId=55490&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-55490
+        if #available(iOS 13.0, *) {
+            return value(forHTTPHeaderField: field)
+        } else {
+            guard let value = (allHeaderFields as NSDictionary)[field] as? String else { return nil }
+            return value
+        }
     }
 }
