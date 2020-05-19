6bb2736880d0cbaa480318b1dbc674fc8d158fe1
Christian Menschel <christian@tapwork.de>
2020-05-19 17:16:26 +0200

    Fix parsing server-timing

--- Tests/InstanaAgentTests/Utils_Tests/Extensions/URLResponseHeaderFieldsTests.swift
+++ Tests/InstanaAgentTests/Utils_Tests/Extensions/URLResponseHeaderFieldsTests.swift
@@ -17,6 +17,46 @@ class URLResponseHeaderFieldsTests: InstanaTestCase {
         AssertEqualAndNotNil(sut, backendTracingID)
     }
 
+    func test_backendTracingID_happy_path_lowercase() {
+        // Given
+        let backendTracingID = "d2f7aebc1ee0813c"
+        let headerFields = ["server-timing": "intid;desc=\(backendTracingID)"]
+        let httpResponse = HTTPURLResponse(url: URL.random, statusCode: 200, httpVersion: "1.1", headerFields: headerFields)
+
+        // When
+        let sut = httpResponse?.backendTracingID
+
+        // Then
+        AssertEqualAndNotNil(sut, backendTracingID)
+    }
+
+    func test_backendTracingID_multiple_values() {
+        // Given
+        let backendTracingID = "d2f7aebc1ee0813c"
+        let headerFields = ["server-timing": "cdn-cache;desc=HIT, intid;desc=\(backendTracingID), edge;dur=1"]
+        let httpResponse = HTTPURLResponse(url: URL.random, statusCode: 200, httpVersion: "1.1", headerFields: headerFields)
+
+        // When
+        let sut = httpResponse?.backendTracingID
+
+        // Then
+        AssertEqualAndNotNil(sut, backendTracingID)
+    }
+
+    func test_backendTracingID_multiple_backendTracingIDs() {
+        // Given
+        let backendTracingID1 = "d2f7aebc1ee0813c"
+        let backendTracingID2 = "anotherID"
+        let headerFields = ["server-timing": "cdn-cache;desc=HIT, intid;desc=\(backendTracingID1), edge;dur=1, intid;desc=\(backendTracingID2)"]
+        let httpResponse = HTTPURLResponse(url: URL.random, statusCode: 200, httpVersion: "1.1", headerFields: headerFields)
+
+        // When
+        let sut = httpResponse?.backendTracingID
+
+        // Then
+        AssertEqualAndNotNil(sut, backendTracingID2)
+    }
+
     func test_backendTracingID_missing() {
         // Given
         let backendTracingID = ""
@@ -32,8 +72,21 @@ class URLResponseHeaderFieldsTests: InstanaTestCase {
 
     func test_backendTracingID_broken() {
         // Given
-        let backendTracingID = ""
+        let backendTracingID = "someID"
         let headerFields = ["Server-Timing": "invalid_desc=\(backendTracingID)"]
+        let httpResponse = HTTPURLResponse(url: URL.random, statusCode: 200, httpVersion: "1.1", headerFields: headerFields)
+
+        // When
+        let sut = httpResponse?.backendTracingID
+
+        // Then
+        XCTAssertNil(sut)
+    }
+
+    func test_backendTracingID_broken_2() {
+        // Given
+        let backendTracingID = "someID"
+        let headerFields = ["ServerTiming": "intid;desc=\(backendTracingID)"]
         let httpResponse = HTTPURLResponse(url: URL.random, statusCode: 200, httpVersion: "1.1", headerFields: headerFields)
 
         // When
