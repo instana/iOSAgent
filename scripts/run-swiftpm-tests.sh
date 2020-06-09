ee5ebaf33071207b3f758ba59713cdc7a8517e68
Christian Menschel <christian@tapwork.de>
2020-06-09 14:07:42 +0200

    Add bitrise.yml and update to Xcode 11.5

--- scripts/run-swiftpm-tests.sh
+++ scripts/run-swiftpm-tests.sh
@@ -27,5 +27,5 @@ echo "Running..."
 case "$ios_version" in
     11)       xcodebuild test -destination 'name=iPhone X,OS=11.1' -scheme ''${scheme}'' | xcpretty "--color";;
     12)       xcodebuild test -destination 'name=iPhone Xs Max,OS=12.4' -scheme ''${scheme}'' | xcpretty "--color";;
-    13)       xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.3' -scheme ''${scheme}'' | xcpretty "--color";;
+    13)       xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.5' -scheme ''${scheme}'' | xcpretty "--color";;
 esac
--- scripts/run-ui-tests.sh
+++ scripts/run-ui-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -o pipefail
 cd Dev
-xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.3' -scheme 'iOSAgentExample'
+xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.5' -scheme 'iOSAgentExample'
