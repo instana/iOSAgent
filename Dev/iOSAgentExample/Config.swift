61775d46156710b4711978afadc709592d9d1af4
Christian Menschel <christian@tapwork.de>
2020-07-20 19:17:31 +0200

    Improve environment variable loading into the config

---
+++ Dev/iOSAgentExample/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  iOSAgentExample
+//
+//  Created by Christian Menschel on 20.07.20.
+//  Copyright © 2020 Instana Inc. All rights reserved.
+//
+
+import Foundation
+
+let InstanaKey = "KEY"
+let InstanaURL = "URL"
