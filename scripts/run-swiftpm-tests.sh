d68da00342ad044ab47ad0036954ccb451fd6da8
Christian Menschel <christian@tapwork.de>
2020-10-22 09:09:34 +0200

    Update bitrise yml and shell script

--- scripts/run-swiftpm-tests.sh
+++ scripts/run-swiftpm-tests.sh
@@ -28,4 +28,6 @@ case "$ios_version" in
     11)       xcodebuild test -destination 'name=iPhone X,OS=11.1' -scheme ''${scheme}'' | xcpretty "--color";;
     12)       xcodebuild test -destination 'name=iPhone Xs Max,OS=12.4' -scheme ''${scheme}'' | xcpretty "--color";;
     13)       xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.5' -scheme ''${scheme}'' | xcpretty "--color";;
+    14)       xcodebuild test -destination 'name=iPhone 12 Pro Max,OS=14.1' -scheme ''${scheme}'' | xcpretty "--color";;
+    *)  echo "iOS $ios_version not defined yet"
 esac
