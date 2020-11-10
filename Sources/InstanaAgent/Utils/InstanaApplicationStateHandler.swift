4537cbdf26cc3869f81ce91f1e8f2c4281d0f694
Christian Menschel <christian@tapwork.de>
2020-11-10 10:46:44 +0100

    Remove UIApplication.shared to make agent usable for extensions

--- Sources/InstanaAgent/Utils/InstanaApplicationStateHandler.swift
+++ Sources/InstanaAgent/Utils/InstanaApplicationStateHandler.swift
@@ -3,24 +3,39 @@ import UIKit
 
 // We need an abstract state handler to make the Notifications (didBecomeActiveNotification, ...) easier to test
 class InstanaApplicationStateHandler {
+
+    enum State {
+        case active
+        case inactive
+        case background
+    }
+
     static let shared = InstanaApplicationStateHandler()
 
-    @Atomic var state: UIApplication.State = UIApplication.shared.applicationState {
+    @Atomic var state: State = .inactive {
         didSet { stateUpdateHandler.forEach { $0(state) } }
     }
 
-    typealias StateUpdater = (UIApplication.State) -> Void
+    typealias StateUpdater = (State) -> Void
     private var stateUpdateHandler = [StateUpdater]()
 
     init() {
         _ = NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification, object: nil, queue: nil) { _ in
             self.state = .active
         }
+        _ = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSExtensionHostDidBecomeActive, object: nil, queue: nil) { _ in
+            self.state = .active
+        }
         _ = NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { _ in
             self.state = .background
         }
-
+        _ = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSExtensionHostDidEnterBackground, object: nil, queue: nil) { _ in
+            self.state = .background
+        }
         _ = NotificationCenter.default.addObserver(forName: UIApplication.willResignActiveNotification, object: nil, queue: nil) { _ in
+            self.state = .inactive
+        }
+        _ = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSExtensionHostWillResignActive, object: nil, queue: nil) { _ in
             self.state = .inactive
         }
     }
