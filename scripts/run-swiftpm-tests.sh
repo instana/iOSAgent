a6cc289d47416f31bab16b0ffd1df26d51eae3d2
Christian Menschel <christian@tapwork.de>
2021-05-05 18:57:14 +0200

    Fix script to run tests

--- scripts/run-swiftpm-tests.sh
+++ scripts/run-swiftpm-tests.sh
@@ -11,7 +11,7 @@ while [ $# -gt 0 ]; do
       ;;
     *)
       printf "* Error: Invalid arguments.*\n"
-      printf "Usage: --ios=[11.4|12.4|13.3|14.4] --test=[unit|integration|ui]\n"
+      printf "Usage: --ios=[11.4|12.4|13.3|14.5] --test=[unit|integration|ui]\n"
       exit 1
   esac
   shift
@@ -28,6 +28,6 @@ case "$ios_version" in
     11)       xcodebuild test -destination 'name=iPhone X,OS=11.1' -scheme ''${scheme}'' | xcpretty "--color";;
     12)       xcodebuild test -destination 'name=iPhone Xs Max,OS=12.4' -scheme ''${scheme}'' | xcpretty "--color";;
     13)       xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.5' -scheme ''${scheme}'' | xcpretty "--color";;
-    14)       xcodebuild test -destination 'name=iPhone 12 Pro Max,OS=14.4' -scheme ''${scheme}'' | xcpretty "--color";;
+    14)       xcodebuild test -destination 'name=iPhone 12 Pro Max,OS=14.5' -scheme ''${scheme}'' | xcpretty "--color";;
     *)  echo "iOS $ios_version not defined yet"
 esac
