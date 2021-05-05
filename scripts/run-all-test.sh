07733deef863443f5582881514c673f014c29bf2
Christian Menschel <christian@tapwork.de>
2021-05-05 22:40:51 +0200

    Clean up test runner scripts

--- scripts/run-all-test.sh
+++ scripts/run-all-test.sh
@@ -1,5 +1,5 @@
 ./scripts/run-swiftpm-tests.sh --ios=14 --test=unit
-./scripts/run-swiftpm-tests.sh --ios=13 --test=unit
-./scripts/run-swiftpm-tests.sh --ios=12 --test=unit
+#./scripts/run-swiftpm-tests.sh --ios=13 --test=unit
+#./scripts/run-swiftpm-tests.sh --ios=12 --test=unit
 
-./scripts/run-swiftpm-tests.sh --ios=14 --test=integration
+#./scripts/run-swiftpm-tests.sh --ios=14 --test=integration
