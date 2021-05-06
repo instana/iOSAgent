d0e19d5298416f722e206503d69f3139a5aacd95
Christian Menschel <christian@tapwork.de>
2021-05-06 09:13:29 +0200

    Improve test script

--- scripts/run-swiftpm-tests.sh
+++ scripts/run-swiftpm-tests.sh
@@ -17,11 +17,11 @@ while [ $# -gt 0 ]; do
 done
 
 
-echo "Running..."
+echo "Starting test suite for iOS ${ios_version}"
 case "$ios_version" in
-    11)       xcodebuild test -destination 'name=iPhone X,OS=11.1' -scheme ''${scheme}'' | xcpretty "--color";;
-    12)       xcodebuild test -destination 'name=iPhone Xs Max,OS=12.4' -scheme ''${scheme}'' | xcpretty "--color";;
-    13)       xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.5' -scheme ''${scheme}'' | xcpretty "--color";;
-    14)       xcodebuild test -destination 'name=iPhone 12 Pro Max,OS=14.5' -scheme ''${scheme}'' | xcpretty "--color";;
+    11)       xcodebuild test -destination 'name=iPhone X,OS=11.1' -scheme ''${scheme}'' | xcpretty --color -t;;
+    12)       xcodebuild test -destination 'name=iPhone Xs Max,OS=12.4' -scheme ''${scheme}'' | xcpretty --color -t;;
+    13)       xcodebuild test -destination 'name=iPhone 11 Pro Max,OS=13.5' -scheme ''${scheme}'' | xcpretty --color -t;;
+    14)       xcodebuild test -destination 'name=iPhone 12 Pro Max,OS=14.5' -scheme ''${scheme}'' | xcpretty --color -t;;
     *)  echo "iOS $ios_version not defined yet"
 esac
