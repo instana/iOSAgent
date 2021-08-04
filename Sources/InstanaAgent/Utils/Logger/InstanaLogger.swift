ed7ea9d34a32bc2ed5fcd0acabe65597a584ec29
Christian Menschel <christian@tapwork.de>
2021-08-04 16:14:14 +0200

    Use os logger

--- Sources/InstanaAgent/Utils/Logger/InstanaLogger.swift
+++ Sources/InstanaAgent/Utils/Logger/InstanaLogger.swift
@@ -3,15 +3,16 @@
 //
 
 import Foundation
+import os
 
 class InstanaLogger {
     enum Level: Int, RawRepresentable {
         case debug, warning, error, none
         fileprivate var logTag: String {
             switch self {
-            case .debug: return "[Debug]"
-            case .warning: return "[Warning]"
-            case .error: return "[Error]"
+            case .debug: return "Debug"
+            case .warning: return "Warning"
+            case .error: return "Error"
             case .none: return ""
             }
         }
@@ -26,7 +27,22 @@ class InstanaLogger {
     func add(_ entry: String, level: Level = .debug) {
         guard level.rawValue >= self.level.rawValue else { return }
         #if DEBUG
-            print("[Instana] \(level.logTag): \(entry)")
+            let message = "[Instana]: \(entry)"
+            if #available(iOS 14.0, *) {
+                let logger = os.Logger(subsystem: "com.instana.ios.agent.logging", category: level.logTag)
+                switch level {
+                case .debug:
+                    logger.debug("\(message)")
+                case .warning:
+                    logger.warning("\(message)")
+                case .error:
+                    logger.error("\(message)")
+                case .none:
+                    break
+                }
+            } else {
+                print(message)
+            }
         #endif
     }
 }
