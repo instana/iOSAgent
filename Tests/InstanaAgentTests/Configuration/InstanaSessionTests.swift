c03e599fa399ec5d2e0f92a249fe1bfa73835b3b
Christian Menschel <christian@tapwork.de>
2021-08-04 16:16:56 +0200

    Rename files properly

--- Tests/InstanaAgentTests/Configuration/InstanaSessionTests.swift
+++ Tests/InstanaAgentTests/Configuration/InstanaSessionTests.swift
@@ -6,7 +6,7 @@ import Foundation
 import XCTest
 @testable import InstanaAgent
 
-class InstanaEnvironmentTests: InstanaTestCase {
+class InstanaSessionTests: InstanaTestCase {
 
     func test_defaultInit() {
         // Given
