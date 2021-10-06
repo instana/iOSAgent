5b7115f229fa668ba5baaa7f20edb55cefe71791
Christian Menschel <christian@tapwork.de>
2021-10-06 17:05:52 +0200

    Update example to demonstrate AFNetworking and Alamofire

--- Dev/InstanaAgentExample/ImageViewController.swift
+++ Dev/InstanaAgentExample/ImageViewController.swift
@@ -7,38 +7,75 @@
 import UIKit
 import Combine
 import InstanaAgent
+import AFNetworking
+import Alamofire
 
 class ImageViewViewController: UIViewController {
 
     lazy var imageView = UIImageView()
     lazy var session = { URLSession(configuration: URLSessionConfiguration.default) }()
     private var publisher: AnyCancellable?
+    private var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .white
         view.addSubview(imageView)
         imageView.contentMode = .scaleAspectFill
-        imageView.frame = view.bounds
-        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.isUserInteractionEnabled = true
         imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(downloadImage)))
 
-        downloadImage()
+        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(downloadImage), userInfo: nil, repeats: true)
+        timer?.fire()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         Instana.setView(name: "ImageView")
-        Instana.reportEvent(name: "Custom Event", duration: 1001, backendTracingID: nil, error: NSError(domain: "Domain", code: 0, userInfo: nil), meta: ["Key": "Val"], viewName:"Image")
+     //   Instana.reportEvent(name: "Custom Event", duration: 1001, backendTracingID: nil, error: NSError(domain: "Domain", code: 0, userInfo: nil), meta: ["Key": "Val"], viewName:"Image")
     }
 
     @objc func downloadImage() {
         let url = URL(string: "https://picsum.photos/900")!
+        let function = [downloadWViaAFN, downloadViaCombine, downloadViaAlamofire].randomElement()!
+        function(url)
+    }
+
+    func downloadViaCombine(_ url: URL) {
         publisher = session.dataTaskPublisher(for: url)
             .receive(on: RunLoop.main)
             .map { UIImage(data: $0.data) }
             .replaceError(with: UIImage())
             .assign(to: \.image, on: self.imageView)
+    }
+
+    func downloadWViaAFN(_ url: URL) {
+        let manager = AFURLSessionManager(sessionConfiguration: .default)
+        manager.responseSerializer = AFHTTPResponseSerializer()
+        let request = URLRequest(url: url)
+        manager.dataTask(with: request, uploadProgress: nil, downloadProgress: nil) { response, data, error in
+            DispatchQueue.main.async {
+                guard let data = data as? Data else { return }
+                self.imageView.image = UIImage(data: data)
+            }
+        }.resume()
+    }
+
+    func downloadViaAlamofire(_ url: URL) {
+        let request = AF.request(url)
+        request.response { result in
+            DispatchQueue.main.async {
+                guard let data = result.data else { return }
+                self.imageView.image = UIImage(data: data)
+            }
+        }
     }
 }
