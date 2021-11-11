8af0336497f468458433607039d7b283c9d8695b
Christian Menschel <christian@tapwork.de>
2021-11-11 14:42:58 +0100

    Remove exclude

--- Package.swift
+++ Package.swift
@@ -20,11 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "InstanaAgent",
-            dependencies: ["Gzip"],
-            exclude: ["Dev"]),
+            dependencies: ["Gzip"]),
         .testTarget(
             name: "InstanaAgentTests",
-            dependencies: ["InstanaAgent"],
-            exclude: ["Dev"]),
+            dependencies: ["InstanaAgent"]),
     ]
 )
